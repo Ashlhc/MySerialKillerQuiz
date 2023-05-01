9034f038882eb9d925dc4fcd87cf8cab42581b38
Ashleigh Chute <ashlhc@uw.edu>
2023-05-01 16:45:30 -0700

    fixed broken JavaScript

--- assets/script.js
+++ assets/script.js
@@ -6,184 +6,241 @@
 //As a user, I want to try and guess the correct answer by pressing a button.
 //  Create an Array for all of the answers
 
-// create variables to reference DOM elements
-    var timeRemaining;
-    var answers;
-//    var startButton;
-    var usersInfo;
-    var saveButton;
-    var highScores;
-    var questions;
-    
-    var things = ["red", "blue", "green"]
-    
-    var buttonPlace = document.getElementById("test")
-    
-    function buttonTime (){
 
-        var button = document.createElement("button")
-        button.textContent = "WOW"
-
-//        buttonPlace.append(button)
-
-
-    }
-
-buttonTime()
-
-//startButton
-const startButton = document.getElementById("btn");
-
-function startQuiz (){
-    console.log("Hello");
-    btn.style.display = 'none';
-    timeMachine()
-}
-startButton.addEventListener("click", startQuiz);
-
-function timeMachine (){
-
-setInterval(displayHello, 1000);
-var count = 69
-function displayHello() {
-var timer = document.getElementById("counter");
-timer.innerHTML = count 
-count = count - 1
-if (count < 0) {
-    ;
-}
-
-}
-}
-//  quiz container
 var questions = [
-        {
-            question1: "Which serial killer stored a body in their bathtub and proceeded to shower over them for a month?",
-            answers: ['1.John Wayne Gacy','2.Jeffrey Dahmer','3.Ted Bundy','4.Gary Ridgway'],
-                // 1: 'John Wayne Gacy',
-                // 2: 'Jeffrey Dahmer',
-                // 3: 'Ted Bundy',
-                // 4: 'Gary Ridgway'
-            // },
-            correctAnswer: '2'
-        },
-        {
-            question2: "Which serial killer helped in the investigation to catch the Green River Killer?",
-            answers: {
-                1: 'Dennis Rader',
-                2: 'Ed Gein',
-                3: 'Aileen Wuornos',
-                4: 'Ted Bundy'
-            },
-            correctAnswer: '4'
-        },
-        {
-            question3: "Which serial killer was known as The Killer Clown?",
-            answers: {
-                1: 'John Wayne Gacy',
-                2: 'H.H. Holmes',
-                3: 'Edmund Kemper',
-                4: 'Rodney Alcala'
-            },
-            correctAnswer: '1'
-        },
-        {
-            question4: "Which serial killer was a professional wrestler and was known as The Old Lady Killer?",
-            answers: {
-                1: 'David Berkowitz',
-                2: 'Aileen Wuornos',
-                3: 'Juan Barraza',
-                4: 'Ted Bundy'
-            },
-            correctAnswer: '3'
-        },
-        {
-            question5: "Which serial killer has confessed to killing as many as 71 women even though only 49 are confirmed",
-            answers: {
-                1: 'Jeffrey Dahmer',
-                2: 'Ted Bundy',
-                3: 'John Wayne Gacy',
-                4: 'Gary Ridgway'
-            },
-            correctAnswer: '4'
-        }
-    ]
+    "Which serial killer stored a body in their bathtub and proceeded to shower over them for a month?",
+    "Which serial killer helped in the investigation to catch the Green River Killer?",
+    "Which serial killer was known as The Killer Clown?",
+    "Which serial killer was a professional wrestler and was known as The Old Lady Killer?",
+    "Which serial killer has confessed to killing as many as 71 women even though only 49 are confirmed?"];
+var options = [
+    [2, 'John Wayne Gacy','Jeffrey Dahmer','Ted Bundy','Gary Ridgway'],
+    [4, 'Dennis Rader','Ed Gein','Aileen Wuornos','Ted Bundy'],
+    [1, 'John Wayne Gacy','H.H. Holmes','Edmund Kemper','Rodney Alcala'],
+    [3, 'David Berkowitz','Aileen Wuornos','Juan Barraza','Ted Bundy'],
+    [4, 'Jeffrey Dahmer','Ted Bundy','John Wayne Gacy','Gary Ridgway']
+];
 
-//  start game
-//      hide start button
-//     const btn = document.getElementById('btn');
-
-// btn.addEventListener('click', () => {
-//     //hide the button
-//     btn.style.display = 'none';
-
-// //As a user, I want the game to be timed.
-//     console.time('timeRemaining');
- 
-//     for (var i = 0; i < 100000000;i++);
- 
-//     console.timeEnd('timeRemaining');
-
-// });
-//      show the quiz container
-var quizContainer =
-document.getElementById('quiz');
-var resultsContainer = 
-document.getElementById('results');
-var submitButton = 
-document.getElementById('submit');
-//      display first question and answers
-var myQuestions = [
-{}
-]
-
-//      each question should be a card 
-//      each answer should be a button
-//      5 separate questions with four answers for each
-
-//      start timer
-//      display countdown on screen
-
-//As a user, I want to win the game when I have guessed the correct answer and clicked the corresponding button.
-//   var handleAnswerClicks = function (event) {
-//     event.preventDefault();
-//     if (question1 = 2)
-//   }
-
-//      handle answer clicks
-//      if
-//      answer is correct  
-//          restart timer
-//          display next question
-//              
-//      if
-//      answer is WRONG
-//          subtract time from timer
-//          make sure time is displayed correctly on page
-//          flash wrong answer message (setTimeout)
-//      
-//     update question variable to next question
-//     display question on page
-//
-//      if
-//      last question is reached
-//      trigger quiz failure
+var highScores = []
+// create variables to reference DOM elements
+var timer = document.querySelector("#timer");
+var startButton = document.querySelector("#start");
+var usersInfo = document.querySelector("#info");
+var optionsField = document.querySelector("#options");
+var replayButton = document.querySelector("#replay");
+var assessment = document.querySelector("#assessment");
+var scoresLink = document.querySelector("#scores-link");
+var playScreen = document.querySelector(".play");
+    
+// Global variables 
+var timeLeft
+var gameTimer
+var place = 0 
+var score = 0
+var outcome
+var pause = false;
 
 //As a user, I want to lose the game when the timer runs out before I have guessed all the answers.
-//      if
-//      timer hits zero
-//          trigger quiz failure
+function startTimer() {
+    timeLeft = 50
+    timer.textContent = timeLeft + "seconds left";
+    gameTimer = setInterval(function(){
+        timeLeft--
+        timer.textContent = timeLeft + "seconds left";
+        if(timeLeft<=0) {
+            timesUp();
+        }
+    },1000)
+}
 
 //As a user, I want to see my total wins and losses on the screen at the end of the quiz. 
-//  function
-//      saving high scores
-//          get value of user input (name/initials)
-//          validate input
-//          retreive existing data from local storage
-//          update win/loss data
-//          save updated data back to local storage
-//          redirect to start screen after save
+function timesUp() {
+    clearInterval(gameTimer)
+    timer.textContent = "Out of time!";
+    outcome = "lose";
+    endGame();
+}
 
+function retrieveScores() {
+    var storeScores = JSON.parse(localStorage.getItem("scores"));
+    if (storeScores !== null) {
+        highScores = storeScores;
+    }
+}
+
+function renderScores() {
+    var scoreList = document.querySelector(".play table");
+
+    highScores.sort((a,b)=> b[1] - a[1])
+    scoreList.innerHTML=""
+
+    usersInfo.setAttribute("style","display:none")
+    startButton.removeAttribute("style");
+    while(document.querySelector(".play .hide")){
+        var hide = document.querySelector(".hide")
+        playScreen.removeChild(hide)
+    }
+
+    for (i=0;i<highScores.length;i++) {
+        var tr = document.createElement("tr")
+        var td1 = document.createElement("td")
+        var td2 = document.createElement("td")
+        tr.setAttribute("class","hide")
+        td1.textContent=highScores[i][0]
+        td1.setAttribute("class","hide")
+        td2.textContent=highScores[i][1]
+        td2.setAttribute("class","hide")
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        scoreList.appendChild(tr)
+    }
+}
+
+function storeScores() {
+    highScores.sort((a,b) => b[1] - a[1])
+    localStorage.setItem("scores",JSON.stringify(highScores))
+}
+
+function renderQs() {
+    optionsField.innerHTML=""
+    usersInfo.removeAttribute("style");
+    startButton.setAttribute("style","display:none");
+
+    if (place >= questions.length) {
+        outcome = "win";
+        endGame();
+    } else {
+        usersInfo.textContent = questions[place]
+        for (i=1;i<options[place].length;i++) {
+            var li = document.createElement("li")
+            li.textContent=options[place][i];
+            li.dataset.index=i;
+            optionsField.appendChild(li)
+        }
+
+        pause = false;
+    }
+}
+
+function endGame() {
+    clearInterval(gameTimer)
+    place = 0;
+    if (outcome === "lose") {
+        startButton.textContent="Try Again!";
+        startButton.removeAttribute("style");
+        optionsField.innerHTML="";
+        usersInfo.textContent = "You Lost!"
+    }else{
+        usersInfo.textContent="You Won!"
+        timer.textContent = "Thank you for playing!"
+        score = timeLeft
+        
+        var scoreInfo = document.createElement("h3")
+        scoreInfo.setAttribute("class", "hide")
+        scoreInfo.textContent = "Your score is: " + score;
+        playScreen.appendChild(scoreInfo);
+
+        var instruct = document.createElement("p")
+        instruct.setAttribute("class","hide")
+        instruct.textContent = "Enter your initials here to save your score for later!"
+        playScreen.appendChild(instruct);
+
+        var scoreForm = document.createElement("form");
+        scoreForm.setAttribute("class","hide")
+
+        var textbox = document.createElement("input");
+        textbox.setAttribute("type","text")
+        textbox.setAttribute("placeholder","Enter your initials!")
+        textbox.setAttribute("minlength","1")
+        textbox.setAttribute("maxlength", "4")
+
+        var submit = document.createElement("input");
+        submit.setAttribute("type","submit")
+        submit.setAttribute("value","Submit")
+        submit.setAttribute("class","submit")
+        scoreForm.appendChild(textbox);
+        scoreForm.appendChild(submit);
+        playScreen.appendChild(scoreForm);
+    }
+}
+
+document.addEventListener("submit",function(event){
+    event.preventDefault();
+    event.target.matches("form")
+
+    retrieveScores();
+
+    var initialsInput = document.querySelector("form input");
+
+    var initials = initialsInput.value.trim();
+    initials = initials.toUpperCase();
+    if (initials === "") {
+        return;
+    }
+
+    var newScore = []
+    newScore.push(initials);
+    newScore.push(score);
+    highScores.push(newScore)
+
+    storeScores();
+    renderScores();
+
+    startButton.textContent= "Play Again?"
+})
+
+optionsField.addEventListener("click",function(event){
+    if (pause) {
+        return;
+    }
+    var answer = event.target;
+    var answersIndex = answer.getAttribute("data-index");
+    var correct = options[place][0]
+
+    if (answer.matches("li")) {
+        if(answersIndex==correct) {
+        answer.setAttribute("class","correct");
+    }else{
+        answer.setAttribute("class","wrong");
+        assessment.textContent="The Correct Answer is: " + options[place][correct]
+        timeLeft -= 10
+        if(timeLeft<=0) {
+            timesUp();
+            return;
+        }else{
+            timer.textContent = timeLeft + " seconds left";
+        }
+    } 
+
+        pause = true;
+        setTimeout(function(){
+            assessment.textContent = ""
+            place++;
+            renderQs();
+        },1000);
+    }
+})
+
+scoresLink.addEventListener("click",function(){
+    clearInterval(gameTimer);
+    place=0
+    timer.textContent = "Press the button to start playing!";
+    assessment.textContent="";
+    optionsField.innerHTML="";
+    retrieveScores();
+    renderScores();
+})
+
+startButton.addEventListener("click",function(){
+    for (i=0;i<highScores.length;i++) {
+        var hide = document.querySelector(".hide")
+        var table = document.querySelector(".play table")
+        table.removeChild(hide)
+    }
+    assessment.textContent=""
+    startTimer();
+    renderQs();
+})
 //### Specifications
 
 //When a user presses a letter key, the user's guess should be captured as a key event.
